b3e7b0c7de1e7dc25c35209df756f24ca5b2d310
Balaram Nadiya <Balaram Nadiya@NADIYA>
2024-12-23 21:16:11 +0530

    Initial Push

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+	public static void main(String[] args) {
+			Dog d=new Dog();
+			d.setName("Snoozy");
+			d.setColor("White");
+			d.setAge(2);
+			d.setCost(10000);
+			d.setBreed("Poodle");
+			System.out.println("Name:"+d.getName());
+			System.out.println("Color:"+d.getColor());
+			System.out.println("Age:"+d.getAge());
+			System.out.println("Cost:"+d.getcost());
+			System.out.println("Breed:"+d.getBreed());
+			}
+}
+
---
+++ src/Dog.java
@@ -0,0 +1,51 @@
+
+public class Dog
+{
+private String name;
+private String color;
+private int age;
+private int cost;
+private String breed;
+
+public void setName(String name)
+{
+this.name=name;	
+}
+public String getName()
+{
+	return name;
+}
+public void setColor(String color)
+{
+this.color=color;	
+}
+public String getColor()
+{
+	return color;
+}
+public void setAge(int age)
+{
+ this.age=age;	
+}
+public int getAge()
+{
+	return age;
+}
+public void setCost(int cost)
+{
+this.cost=cost;	
+}
+public int getcost()
+{
+	return cost;
+}
+public void setBreed(String breed)
+{
+this.breed=breed;	
+}
+public String getBreed()
+{
+	return breed;
+}
+
+}
